2a94c51bce5bf4425180af675eef66ad65ed1ce6
Valmir Moro Conque Filho <vmcfilho@gmail.com>
2024-03-16 16:44:11 -0300

    first commit

---
+++ src/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './Login.css'; // Importando o CSS
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  const validarLogin = () => {
+    if (email === 'vmcfilho@gmail.com' && senha === '123456') {
+      setMensagem('Acessado com sucesso!');
+    } else {
+      setMensagem('Usuário ou senha incorretos!');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <input
+        type="email"
+        placeholder="E-mail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        value={senha}
+        onChange={(e) => setSenha(e.target.value)}
+      />
+      <button onClick={validarLogin}>Acessar</button>
+      <label>{mensagem}</label>
+    </div>
+  );
+}
+
+export default Login;
